1400c2a1b32c42f6469ae9595947e447e288a7ed
Earl John Pulido <earljohn.pulido@gfitftlapt02381.ad.mynt.xyz>
2025-06-30 17:02:19 +0800

    Final Submission, Lab 4: Area Calculator

---
+++ README.md
@@ -0,0 +1,25 @@
+## Lab 4: Area Calculator
+Write a Java program that calculates the area of different shapes using methods/functions.
+Your program should be able to compute the following:
+
+The area of a circle
+
+The area of a triangle
+
+The area of a rectangle
+
+## Sample Outputs
+
+### Circle
+
+![img.png](img.png)
+
+### Triangle
+
+![img_1.png](img_1.png)
+
+### Rectangle
+
+![img_2.png](img_2.png)
+
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,60 @@
 package org.example;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
 
+import java.util.Scanner;
+
+public class Main {
+    public static double calculateCircleArea(double radius) {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    public static double calculateTriangleArea(double base, double height) {
+        return (base * height) / 2.0;
+    }
+
+    public static double calculateRectangleArea(double length, double width) {
+        return length * width;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print(
+                "=== Area Calculator ===\n" +
+                        "1. Circle\n" +
+                        "2. Triangle\n" +
+                        "3. Rectangle\n" +
+                        "Choose a shape (1-3): "
+        );
+        int shapeInput = scanner.nextInt();
+
+        switch (shapeInput) {
+            case 1:
+                System.out.print("Enter the radius: ");
+                double radiusInput = scanner.nextDouble();
+                double circleArea = calculateCircleArea(radiusInput);
+                System.out.printf("Area of the circle: %.2f%n", circleArea);
+                break;
+
+            case 2:
+                System.out.print("Enter the base: ");
+                double baseInput = scanner.nextDouble();
+                System.out.print("Enter the height: ");
+                double heightInput = scanner.nextDouble();
+                double triangleArea = calculateTriangleArea(baseInput, heightInput);
+                System.out.printf("Area of the triangle: %.2f%n", triangleArea);
+                break;
+
+            case 3:
+                System.out.print("Enter the length: ");
+                double lengthInput = scanner.nextDouble();
+                System.out.print("Enter the width: ");
+                double widthInput = scanner.nextDouble();
+                double rectangleArea = calculateRectangleArea(lengthInput, widthInput);
+                System.out.printf("Area of the rectangle: %.2f%n", rectangleArea);
+                break;
+
+            default:
+                System.out.println("Enter correct value!");
+        }
     }
 }
